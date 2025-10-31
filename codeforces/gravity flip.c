1c12347163d1e261f2b1f825332e8b8188f559ba
Joana da Costa <joanandre.dacosta@gmail.com>
2025-10-31 19:43:53 +0100

    gravity

---
+++ codeforces/gravity flip.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main()
+{
+    int n = 0;
+    int a = 0;
+
+    scanf("%d", &n);
+        int s[n];
+    int i = 0;
+    while(i < n)
+    {
+        scanf("%d", &s[i]);
+        i++;
+    }
+    int j = 0;
+    while(j < n - 1)
+    {
+        i = 0;
+        while(i < n - 1)
+        {
+            if(s[i] > s[i + 1])
+            {
+                a = s[i];
+                s[i] = s[i + 1];
+                s[i + 1] = a;
+            }
+            i++;
+        }
+        j++;
+    }
+    i = 0;
+    while(i < n)
+    {
+        printf("%d ", s[i]);
+        i++;
+    }
+    printf("\n");
+    return(0);
+}
